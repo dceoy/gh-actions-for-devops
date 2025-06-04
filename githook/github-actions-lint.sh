e6eabb4ccdd08b61640bda9b5c276ec367a0e5ff
dceoy <dceoy@users.noreply.github.com>
2025-06-05 01:11:05 +0900

    Remove unused githook scripts

--- githook/github-actions-lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euox pipefail
-
-find .github/workflows -type f \( -name '*.yml' -o -name '*.yaml' \) -print0 \
-  | xargs -0 -t actionlint
-find .github/workflows -type f \( -name '*.yml' -o -name '*.yaml' \) -print0 \
-  | xargs -0 -t yamllint -d '{"extends": "relaxed", "rules": {"line-length": "disable"}}'
--- githook/terraform-format-and-lint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euox pipefail
-
-terraform fmt -recursive . && terragrunt hclfmt --diff --working-dir .
-tflint --recursive --chdir=.
-trivy filesystem --scanners vuln,secret,misconfig .
